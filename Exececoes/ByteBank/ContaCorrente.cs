8231638cb3017426f8dc699a861fb55289ba3d2d
natandovale <natandovale496@gmail.com>
2021-10-06 18:11:52 -0300

    Iniciando excecoes

---
+++ Exececoes/ByteBank/ContaCorrente.cs
@@ -0,0 +1,90 @@
+﻿// using _05_ByteBank;
+
+using System;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        public int Agencia { get; }
+        public int Numero { get;}
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (agencia <= 0)
+            {
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
+            }
+
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor de saque não pode ser negativo", nameof(valor));
+            }
+
+            if (_saldo < valor)
+            {
+                throw new SaldoInsuficienteException(_saldo, valor);
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
+            }
+
+            Sacar(valor);
+            contaDestino.Depositar(valor);
+        }
+    }
+}
---
+++ Exececoes/ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(0, 0);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ParamName);
+            }
+        }
+    }
+}
