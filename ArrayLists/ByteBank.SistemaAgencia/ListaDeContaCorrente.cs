bf6bce1af6266b665d5f2e5d816b28a19a8fda5b
natandovale <natandovale496@gmail.com>
2021-10-12 14:03:44 -0300

    Fazendo uma lista com arrays

---
+++ ArrayLists/ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,50 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+
+        public ListaDeContaCorrente()
+        {
+            _itens = new ContaCorrente[5];
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+            Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+            Console.WriteLine("Aumentando capacidade da lista!");
+
+            for (int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+            }
+
+            _itens = novoArray;
+        }
+    }
+
+}
+
--- ArrayLists/ByteBank.SistemaAgencia/Program.cs
+++ ArrayLists/ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,52 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 5679754));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            lista.Adicionar(new ContaCorrente(874, 5679745));
+            //e assim por diante
+            Console.ReadLine();
+        }
+        static void Arrays()
+        {
+            //Arrays
+            int[] idades = new int[5];
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+                Console.WriteLine($"Valor no índice {indice}: {idade}");
+            }
+        }
+
+        static void TestaArraydeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+                {
+                    new ContaCorrente(874, 5679787),
+                    new ContaCorrente(874, 4456668),
+                    new ContaCorrente(874, 7781438),
+                };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+
             Console.ReadLine();
         }
     }
