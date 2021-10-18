8f57a9466c1a08810bd04df2f11df82ff03d6622
natandovale <natandovale496@gmail.com>
2021-10-18 13:24:58 -0300

    ListT e metodo de extensao

---
+++ list-lambda-link/Projeto/Projeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>();
+
+            idades.Add(1);
+            idades.Add(5);
+            idades.Add(14);
+            idades.Add(25);
+            idades.Add(38);
+            idades.Add(61);
+
+            idades.Remove(5);
+
+            idades.AdicionarVarios(2, 6, 15, 26, 39, 62);
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+        }
+    }
+}
