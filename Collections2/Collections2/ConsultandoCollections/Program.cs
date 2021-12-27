25b270253efa913ee030706718513dec31057e27
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-27 09:01:16 -0300

    Consultando sequencia de elementos

---
+++ Collections2/Collections2/ConsultandoCollections/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsultandoCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROBLEMA: obter os nomes dos meses do ano, que tem 31 dias, em maiúsculo e em ordem alfabética
+            //Janeiro 31
+            //Fevereiro 28
+            //Março 31
+            //Abril 30
+            //Maio 31
+            //Junho 30
+            //Julho 31
+            //Agosto 31
+            //Setembro 30
+            //Outubro 31
+            //Novembro 30
+            //Dezembro 31
+
+            List<Mes> meses = new List<Mes>
+            {
+               new Mes("Janeiro ",31),
+               new Mes("Fevereiro",28),
+               new Mes("Março    ",31),
+               new Mes("Abril    ",30),
+               new Mes("Maio     ",31),
+               new Mes("Junho    ",30),
+               new Mes("Julho    ",31),
+               new Mes("Agosto   ",31),
+               new Mes("Setembro ",30),
+               new Mes("Outubro  ",31),
+               new Mes("Novembro ",30),
+               new Mes("Dezembro ",31)
+            };
+
+            //meses.Sort();
+            //foreach (var mes in meses)
+            //{
+            //    if (mes.Dias == 31)
+            //    {
+            //        Console.WriteLine(mes.Nome.ToUpper());
+            //    }
+            //}
+
+            IEnumerable<string>
+            consulta = meses
+                            .Where(m => m.Dias == 31)
+                            .OrderBy(m => m.Nome)
+                            .Select(m => m.Nome.ToUpper());
+
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public class Mes : IComparable
+    {
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+        public override string ToString()
+        {
+            return $"{Nome} -{Dias}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Mes outro = obj as Mes;
+
+            return this.Nome.CompareTo(outro.Nome);
+        }
+    }
+}
---
+++ Collections2/Collections2/ConsultasSequenciasDeElementos/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsultasSequenciasDeElementos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mounth> meses = new List<Mounth>
+            {
+               new Mounth("Janeiro ",31),
+               new Mounth("Fevereiro",28),
+               new Mounth("Março    ",31),
+               new Mounth("Abril    ",30),
+               new Mounth("Maio     ",31),
+               new Mounth("Junho    ",30),
+               new Mounth("Julho    ",31),
+               new Mounth("Agosto   ",31),
+               new Mounth("Setembro ",30),
+               new Mounth("Outubro  ",31),
+               new Mounth("Novembro ",30),
+               new Mounth("Dezembro ",31)
+            };
+
+            //Pegar o primeiro trimestre
+            var consulta = meses.Take(3);
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public class Mounth : IComparable
+        {
+            public string Nome { get; private set; }
+            public int Dias { get; private set; }
+
+            public Mounth(string nome, int dias)
+            {
+                Nome = nome;
+                Dias = dias;
+            }
+            public override string ToString()
+            {
+                return $"{Nome} -{Dias}";
+            }
+
+            public int CompareTo(object obj)
+            {
+                Mounth outro = obj as Mounth;
+
+                return this.Nome.CompareTo(outro.Nome);
+            }
+        }
+    }
+}
