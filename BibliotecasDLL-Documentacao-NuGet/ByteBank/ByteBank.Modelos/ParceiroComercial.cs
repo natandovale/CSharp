e5b6027e17fca7739d29602b3386ae4c3386ac97
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-01-20 17:04:42 -0300

    Bibliotecas

--- BibliotecasDLL-Documentacao-NuGet/ByteBank/ByteBank.Modelos/ParceiroComercial.cs
+++ BibliotecasDLL-Documentacao-NuGet/ByteBank/ByteBank.Modelos/ParceiroComercial.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ByteBank.Modelos
 {
     public class ParceiroComercial : IAutenticavel
     {
+        private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
         public string Senha { get; set; }
 
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return _autenticacaoHelper.CompararSenhas(Senha, senha);
         }
     }
 }
