760698035d8f68f7b97b4783f573d63a9a2266da
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-03-15 16:03:53 -0300

    Adicionando exemplos

--- String/String/String.Url/ExtratorValorDeArgumentosURL.cs
+++ String/String/String.Url/ExtratorValorDeArgumentosURL.cs
@@ -1,6 +1,36 @@
 ﻿namespace String.Url
 {
-    internal class ExtratorValorDeArgumentosURL
+    public class ExtratorValorDeArgumentosURL
     {
+        private readonly string _argumentos;
+        public string URL { get; }
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if (string.IsNullOrEmpty(url)) throw new ArgumentNullException("url");
+
+            var indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            URL = url;
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            nomeParametro = nomeParametro.ToUpper();
+            string argumentoEmCaixaAlta = _argumentos.ToUpper();
+            string termo = nomeParametro + "=";
+            int indiceTermo = argumentoEmCaixaAlta.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if(indiceEComercial == -1)
+            {
+                return resultado;
+            }    
+
+            return resultado.Remove(indiceEComercial);
+        }
     }
 }
--- String/String/String.Url/Program.cs
+++ String/String/String.Url/Program.cs
@@ -1,5 +1,29 @@
-﻿var url = "pagina?argumentos";
-var indiceInterrogacao = url.IndexOf('?');
-var argumentos = url.Substring(indiceInterrogacao+1);
-Console.WriteLine(argumentos);
-Console.WriteLine(indiceInterrogacao);
+﻿using System.Text.RegularExpressions;
+using String.Url;
+
+var url = "http://www.bytebank.com.br/cambio?moedaOrigem=real&moedaDestino=dolar";
+var extrator = new ExtratorValorDeArgumentosURL(url);
+
+var valor1 = extrator.GetValor("moedaOrigem");
+Console.WriteLine(valor1);
+
+var valor2 = extrator.GetValor("moedaDestino");
+Console.WriteLine(valor2);
+
+/// <summary>
+/// ///////////////////////////////////
+/// </summary>
+/// 
+
+
+//string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+//string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+//string padrao = "[0-9]{4}[-][0-9]{4}";
+//string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+string padrao = "[0-9]{4,5}-?[0-9]{4}";
+string texto = "Meu número é: 2342-3453";
+
+Match match = Regex.Match(texto, padrao);
+Console.WriteLine(match.Value);
+
+Console.ReadLine();
